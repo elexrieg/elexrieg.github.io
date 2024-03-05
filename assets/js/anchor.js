dee253b502adff09a1d194cbd5d3fce02e0a2bef
bsod2528 <bsod2528@proton.me>
2024-03-05 22:17:41 +0530

    chore: remove unwanted

--- assets/js/anchor.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('h1, h2, h3, h4, h5, h6').each(function() {
-        var id = $(this).attr('id');
-        if (id) {
-            var anchor = $('<a/>').addClass('anchor').attr('href', ' #' + id).text('#');
-            $(this).append(anchor);
-        }
-    });
-});
